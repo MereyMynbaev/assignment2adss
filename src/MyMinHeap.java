4392937ec3397b7fcebf18061d9190dc966bee4c
MereyMynbaev <154600199+MereyMynbaev@users.noreply.github.com>
2024-04-27 09:25:00 +0500

    Update MyMinHeap.java

--- src/MyMinHeap.java
+++ src/MyMinHeap.java
@@ -1,70 +1,112 @@
-public class MyMinHeap {
-    private MyArrayList<Integer> heap;
+import java.util.Comparator;
 
+public class MyMinHeap<T extends Comparable<T>> {
+    private MyArrayList<T> heap;
+    private Comparator<T> comparator;
+
+    // Constructor
     public MyMinHeap() {
-        heap = new MyArrayList<>();
+        this(null);
     }
 
-    public void insert(int item) {
-        heap.addElement(item);
-        heapifyUp(heap.getSize() - 1);
+    // Constructor with custom comparator
+    public MyMinHeap(Comparator<T> comparator) {
+        this.heap = new MyArrayList<>();
+        this.comparator = comparator;
     }
 
-    public int deleteMin() {
+    // Check if the heap is empty
+    public boolean isEmpty() {
+        return heap.size() == 0;
+    }
+
+    // Get the size of the heap
+    public int size() {
+        return heap.size();
+    }
+
+    // Get the minimum element (root of the heap)
+    public T getMin() {
         if (isEmpty()) {
-            throw new RuntimeException("Heap is empty");
+            throw new IllegalStateException("Heap is empty");
         }
-        int min = heap.getElement(0);
-        int lastItem = heap.removeElement(heap.getSize() - 1);
-        if (!isEmpty()) {
-            heap.setElement(0, lastItem);
-            heapifyDown(0);
+        return heap.get(0);
+    }
+
+    // Extract the minimum element from the heap
+    public T extractMin() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Heap is empty");
         }
+        T min = getMin();
+        heap.set(0, heap.get(heap.size() - 1));
+        heap.remove(heap.size() - 1);
+        heapify(0);
         return min;
     }
 
-    public int getMin() {
-        if (isEmpty()) {
-            throw new RuntimeException("Heap is empty");
+    // Insert an element into the heap
+    public void insert(T element) {
+        heap.add(element);
+        traverseUp(heap.size() - 1);
+    }
+
+    // Maintain heap property from the given index downward
+    private void heapify(int index) {
+        int left = leftChildOf(index);
+        int right = rightChildOf(index);
+        int smallest = index;
+
+        if (left < heap.size() && compare(heap.get(left), heap.get(index)) < 0) {
+            smallest = left;
         }
-        return heap.getElement(0);
-    }
 
-    public boolean isEmpty() {
-        return heap.getSize() == 0;
-    }
+        if (right < heap.size() && compare(heap.get(right), heap.get(smallest)) < 0) {
+            smallest = right;
+        }
 
-    private void heapifyUp(int index) {
-        int parentIndex = (index - 1) / 2;
-        while (index > 0 && heap.getElement(index) < heap.getElement(parentIndex)) {
-            heap.swap(index, parentIndex);
-            index = parentIndex;
-            parentIndex = (index - 1) / 2;
+        if (smallest != index) {
+            swap(index, smallest);
+            heapify(smallest);
         }
     }
 
-    private void heapifyDown(int index) {
-        int leftChildIndex;
-        int rightChildIndex;
-        int smallerChildIndex;
+    // Maintain heap property from the given index upward
+    private void traverseUp(int index) {
+        while (index != 0 && compare(heap.get(index), heap.get(parentOf(index))) < 0) {
+            swap(index, parentOf(index));
+            index = parentOf(index);
+        }
+    }
 
-        while (index < heap.getSize() / 2) {
-            leftChildIndex = 2 * index + 1;
-            rightChildIndex = 2 * index + 2;
+    // Get the index of the left child of the given index
+    private int leftChildOf(int index) {
+        return 2 * index + 1;
+    }
 
-            if (rightChildIndex < heap.getSize() &&
-                    heap.getElement(rightChildIndex) < heap.getElement(leftChildIndex)) {
-                smallerChildIndex = rightChildIndex;
-            } else {
-                smallerChildIndex = leftChildIndex;
-            }
+    // Get the index of the right child of the given index
+    private int rightChildOf(int index) {
+        return 2 * index + 2;
+    }
 
-            if (heap.getElement(index) <= heap.getElement(smallerChildIndex)) {
-                break;
-            }
+    // Get the index of the parent of the given index
+    private int parentOf(int index) {
+        return (index - 1) / 2;
+    }
 
-            heap.swap(index, smallerChildIndex);
-            index = smallerChildIndex;
+    // Swap elements at two indices in the heap
+    private void swap(int index1, int index2) {
+        T temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
+
+    // Compare two elements using comparator if available, else use natural ordering
+    private int compare(T first, T second) {
+        if (comparator != null) {
+            return comparator.compare(first, second);
+        } else {
+            return first.compareTo(second);
         }
     }
 }
